4a511d7f49b3ed112134506cda46b7407db8116b
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-17 12:40:23 +0300

    resolved change requests

--- day2-exercises.js
+++ day2-exercises.js
@@ -9,14 +9,16 @@ console.log(`Hi, my name is ${name} and I am ${age} years old, my favorite color
 var a = 5;
 var b = 5;
 
-console.log(
-    `Sum is: ${a + b} \nDifference is: ${a - b}\nProduct is: ${a * b}\nQuotient is: ${a / b}`
-);
+console.log(`Sum is: ${a + b}`);
+console.log(`Difference is: ${a - b}`);
+console.log(`Product is: ${a * b}`);
+console.log(`Quotient is: ${a / b}`);
 
 // 3. Temperature Converter 
 var celsius = 35; 
+var formula = (celsius * 9 / 5) + 32; 
 
-console.log(`The converted temperture from celsius '${celsius}℃' in Fahrenheit is: ${(celsius * 9 / 5) + 32}℉`);
+console.log(`The converted temperture from celsius '${celsius} ℃' in Fahrenheit is: ${formula} ℉`);
 
 //4. String Concatenation
 var firstName = "Victor"; 
@@ -25,6 +27,3 @@ var lastName = "Purice";
 var fullName = firstName + " " + lastName
 
 console.log(`Hi, my name is ${fullName}, nice to meet you.`)
-
-
-
