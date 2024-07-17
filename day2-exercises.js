46c0ae0d1a3d6e8cd20c705d0bf345cbb37335fc
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-17 10:36:12 +0300

    solved Ex.2 Simple Calculator

--- day2-exercises.js
+++ day2-exercises.js
@@ -4,3 +4,14 @@ var age = 20;
 var favoriteColor = "blue"; 
 
 console.log(`Hi, my name is ${name} and I am ${age} years old, my favorite color is ${favoriteColor}.`); 
+
+// 2.Simple Calculator
+var a = 5;
+var b = 5;
+
+console.log(
+    `Sum is: ${a + b} \nDifference is: ${a - b}\nProduct is: ${a * b}\nQuotient is: ${a / b}`
+);
+
+
+
