38de7b5a56593d8221b74144f34a099a59159382
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-17 10:45:28 +0300

    solved Ex.3 Temperature Converter

--- day2-exercises.js
+++ day2-exercises.js
@@ -5,13 +5,18 @@ var favoriteColor = "blue";
 
 console.log(`Hi, my name is ${name} and I am ${age} years old, my favorite color is ${favoriteColor}.`); 
 
-// 2.Simple Calculator
+// 2. Simple Calculator
 var a = 5;
 var b = 5;
 
 console.log(
     `Sum is: ${a + b} \nDifference is: ${a - b}\nProduct is: ${a * b}\nQuotient is: ${a / b}`
 );
+
+// 3. Temperature Converter 
+var celsius = 35; 
+
+console.log(`The converted temperture from celsius '${celsius}℃' in Fahrenheit is: ${(celsius * 9 / 5) + 32}℉`);
 
 
 
