0b587fcd9b9dcf3f848aee8196d35dde52467552
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-17 10:26:35 +0300

    solved Ex.1 Variable Practice

---
+++ day2-exercises.js
@@ -0,0 +1,6 @@
+// 1. Variable Practice
+var name = "Victor"; 
+var age = 20; 
+var favoriteColor = "blue"; 
+
+console.log(`Hi, my name is ${name} and I am ${age} years old, my favorite color is ${favoriteColor}.`); 
