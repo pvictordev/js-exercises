a1c79d83a489aea716159a7c49f8776834207fae
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-17 15:53:25 +0300

    solved Ex.1 Let and Const Practice

---
+++ day3-exercises.js
@@ -0,0 +1,9 @@
+// 1. Let and Const Practice
+let x = 10;
+let y = 5;
+
+const sum = x + y; 
+const difference = x - 7; 
+
+console.log(`Sum is: ${sum}`); 
+console.log(`Difference is: ${difference}`); 
