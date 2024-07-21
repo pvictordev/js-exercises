fd7f294abec3220544c6d6b8130a94b2c3111fd0
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-21 12:36:08 +0300

    solved Ex.2 Object Creation

--- day4-exercises.js
+++ day4-exercises.js
@@ -7,3 +7,21 @@ favoriteFruits.unshift(banana);
 
 favoriteFruits.pop();
 
+console.log(favoriteFruits);
+
+
+// 2. Object Creation
+const book = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018,
+    isAvailable: false
+}
+
+function describeBook(book) {
+    const availability = book.isAvailable ? "available" : "unavailable";
+
+    return `The book "${book.title}", written by ${book.author} and published in ${book.year}, is currently ${availability} in our stores.`;
+}
+
+console.log(describeBook(book));
