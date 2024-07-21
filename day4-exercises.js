339aeb266dd954bceed40355f94aac5efccb86e3
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-21 12:26:56 +0300

    solved Ex.1 Array Manipulation

---
+++ day4-exercises.js
@@ -0,0 +1,9 @@
+// 1. Array Manipulation
+const banana = ("b" + "a" + + "a" + "a").toLowerCase();
+const favoriteFruits = ["pineapple", "apple", "watermelon"];
+
+favoriteFruits.push("mongo");
+favoriteFruits.unshift(banana);
+
+favoriteFruits.pop();
+
